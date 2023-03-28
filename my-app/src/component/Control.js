f5ee5983dac8ed8cbadec53ceb8a951429fda630
Marine <marine09028@gmail.com>
2023-03-28 09:10:43 +0800

    control add hook

--- my-app/src/component/Control.js
+++ my-app/src/component/Control.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function Control() {
+  const [list,setList]=useState([])
+  const onAdd=(value)=> {
+    const newArray = [...list,value]
+    setList(newArray)
+  }
   return (
     <>
     <div className='d-flex justify-content-center mb-4'>
-    <input type="text"/>
-      <button className='ms-2'>提交</button>
+      <input type="text"/>
+      <button className='ms-2' onClick={onAdd()}>提交</button>
     </div>
       
     </>
