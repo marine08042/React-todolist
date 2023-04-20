4f1d56daa9c87e2cac1c01fe6ed2fd709f2616b5
Marine <marine09028@gmail.com>
2023-04-20 22:59:19 +0800

    更改首頁

--- my-app/src/page/Index.js
+++ my-app/src/page/Index.js
@@ -1,20 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Login from "../component/Login";
+import '../component/css/todo.css'
 
 function Index() {
   return (
     <>
-      <div className="d-flex mx-5 mt-5">
-        <div className="text-center w-50 d-flex flex-column justify-content-center">
-          <h1 className="text-primary mb-4">Welcome TODO!</h1>
-          <div className="d-flex justify-content-center align-items-center">
-              <h6 className="m-0">還沒註冊？</h6>
-              <button type="button" className="btn btn-outline-primary h-100 ms-3">
-                前往註冊
-              </button>
-          </div>
-        </div>
-        <Login />
+      {/* <img src={require("../img/banner1.jpeg")} alt="" className="w-100 banner"/> */}
+      <div className="banner d-flex flex-column justify-content-center align-items-center">
+        <div className="banner-title">What do you want TODO?</div>
       </div>
     </>
   );
